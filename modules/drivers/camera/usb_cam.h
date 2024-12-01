182f9394f180a606652e9451a6e0859ae50e62e8
limengyang <limengyang1995@126.com>
2024-12-01 13:43:12 +0800

    camera accelerator

--- modules/drivers/camera/usb_cam.h
+++ modules/drivers/camera/usb_cam.h
@@ -63,8 +63,10 @@ extern "C" {
 #include <string>
 
 #include "cyber/cyber.h"
-
+// #if GPU_PLATFORM == NVIDIA
+// #include <cuda_runtime_api.h>
 #include "modules/drivers/camera/proto/config.pb.h"
+
 
 namespace apollo {
 namespace drivers {
@@ -80,97 +82,100 @@ using apollo::drivers::camera::config::YUYV;
 
 // camera raw image struct
 struct CameraImage {
-  int width;
-  int height;
-  int bytes_per_pixel;
-  int image_size;
-  int is_new;
-  int tv_sec;
-  int tv_usec;
-  char* image;
+    int width;
+    int height;
+    int bytes_per_pixel;
+    int image_size;
+    int is_new;
+    int tv_sec;
+    int tv_usec;
+    char* image;
 
-  ~CameraImage() {
-    if (image != nullptr) {
-      free(reinterpret_cast<void*>(image));
-      image = nullptr;
+    ~CameraImage() {
+        if (image != nullptr) {
+            free(reinterpret_cast<void*>(image));
+            image = nullptr;
+        }
     }
-  }
 };
 
 typedef std::shared_ptr<CameraImage> CameraImagePtr;
 
 struct buffer {
-  void* start;
-  size_t length;
+    void* start;
+    size_t length;
 };
 
 class UsbCam {
- public:
-  UsbCam();
-  virtual ~UsbCam();
+public:
+    UsbCam();
+    virtual ~UsbCam();
 
-  virtual bool init(const std::shared_ptr<Config>& camera_config);
-  // user use this function to get camera frame data
-  virtual bool poll(const CameraImagePtr& raw_image, 
-                    const CameraImagePtr& sensor_raw_image);
+    virtual bool init(const std::shared_ptr<Config>& camera_config);
+    // user use this function to get camera frame data
+    virtual bool poll(const CameraImagePtr& raw_image, const CameraImagePtr& sensor_raw_image);
 
-  bool is_capturing();
-  bool wait_for_device(void);
+    bool is_capturing();
+    bool wait_for_device(void);
 
- private:
-  int xioctl(int fd, int request, void* arg);
-  bool init_device(void);
-  bool uninit_device(void);
+private:
+    int xioctl(int fd, int request, void* arg);
+    bool init_device(void);
+    bool uninit_device(void);
 
-  void set_device_config();
-  // enables/disable auto focus
-  void set_auto_focus(int value);
-  // set video device parameters
-  void set_v4l_parameter(const std::string& param, int value);
-  void set_v4l_parameter(const std::string& param, const std::string& value);
+    void set_device_config();
+    // enables/disable auto focus
+    void set_auto_focus(int value);
+    // set video device parameters
+    void set_v4l_parameter(const std::string& param, int value);
+    void set_v4l_parameter(const std::string& param, const std::string& value);
 
-  int init_mjpeg_decoder(int image_width, int image_height);
-  void mjpeg2rgb(char* mjepg_buffer, int len, char* rgb_buffer, int pixels);
-#ifdef __aarch64__
-  int convert_yuv_to_rgb_pixel(int y, int u, int v);
-  int convert_yuv_to_rgb_buffer(unsigned char* yuv, unsigned char* rgb,
-                                unsigned int width, unsigned int height);
-#endif
+    int init_mjpeg_decoder(int image_width, int image_height);
+    void mjpeg2rgb(char* mjepg_buffer, int len, char* rgb_buffer, int pixels);
+    #ifdef __aarch64__
+      int convert_yuv_to_rgb_pixel_cuda(int y, int u, int v);
+      int convert_yuv_to_rgb_buffer_cuda(unsigned char* yuv, unsigned char* rgb,
+                                    unsigned int width, unsigned int height);
+    #endif
 
-  bool init_read(unsigned int buffer_size);
-  bool init_mmap(void);
-  bool init_userp(unsigned int buffer_size);
-  bool set_adv_trigger(void);
-  bool close_device(void);
-  bool open_device(void);
-  bool read_frame(CameraImagePtr raw_image, CameraImagePtr sensor_raw_image);
-  bool process_image(void* src, int len, CameraImagePtr dest);
-  bool start_capturing(void);
-  bool stop_capturing(void);
-  void reconnect();
-  void reset_device();
+    bool init_read(unsigned int buffer_size);
+    bool init_mmap(void);
+    bool init_userp(unsigned int buffer_size);
+    bool set_adv_trigger(void);
+    bool close_device(void);
+    bool open_device(void);
+    bool read_frame(CameraImagePtr raw_image, CameraImagePtr sensor_raw_image);
+    bool process_image(void* src, int len, CameraImagePtr dest);
+    bool process_image_cuda(void* src, int len, CameraImagePtr dest);
+    bool start_capturing(void);
+    bool stop_capturing(void);
+    void reconnect();
+    void reset_device();
 
-  std::shared_ptr<Config> config_;
-  int pixel_format_;
-  int fd_;
-  buffer* buffers_;
-  unsigned int n_buffers_;
-  bool is_capturing_;
-  uint64_t image_seq_;
+    char* image_yuyv_cuda_ = NULL;
+    char* image_rgb_cuda_ = NULL;
 
-  AVFrame* avframe_camera_;
-  AVFrame* avframe_rgb_;
-  AVCodec* avcodec_;
-  AVDictionary* avoptions_;
-  AVCodecContext* avcodec_context_;
-  int avframe_camera_size_;
-  int avframe_rgb_size_;
-  struct SwsContext* video_sws_;
+    std::shared_ptr<Config> config_;
+    int pixel_format_;
+    int fd_;
+    buffer* buffers_;
+    unsigned int n_buffers_;
+    bool is_capturing_;
+    uint64_t image_seq_;
 
-  float frame_warning_interval_ = 0.0;
-  float device_wait_sec_ = 0.0;
-  uint64_t last_nsec_ = 0;
-  float frame_drop_interval_ = 0.0;
+    AVFrame* avframe_camera_;
+    AVFrame* avframe_rgb_;
+    AVCodec* avcodec_;
+    AVDictionary* avoptions_;
+    AVCodecContext* avcodec_context_;
+    int avframe_camera_size_;
+    int avframe_rgb_size_;
+    struct SwsContext* video_sws_;
+
+    float frame_warning_interval_ = 0.0;
+    float device_wait_sec_ = 0.0;
+    uint64_t last_nsec_ = 0;
+    float frame_drop_interval_ = 0.0;
 };
 }  // namespace camera
 }  // namespace drivers
