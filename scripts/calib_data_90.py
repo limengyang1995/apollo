2b03172716e4159fac6ee4f18e62229009d50d13
limengyang1995 <limengyang1995@126.com>
2025-02-19 10:06:21 +0800

    refine calib scritpts

--- scripts/calib_data_90.py
+++ scripts/calib_data_90.py
@@ -25,35 +25,39 @@ def convert_frd_to_flu_rotation_matrix(frd_matrix):
     return flu_matrix_yaw90
 
 def convert_odometry_to_lidar_calib(input_file, output_file):
+    j = 0
     with open(input_file, 'r') as f:
         lines = f.readlines()
-
     with open(output_file, 'w') as f_out:
         for i, line in enumerate(lines):
-            data = line.strip().split()
-            index = i + 1  # 第一列是从1开始的计数
-            timestamp = float(data[1])
-            x = float(data[2])  # UTM x
-            y = float(data[3])  # UTM y
-            z = float(data[4])  # UTM z
-            qx = float(data[5])  # qx
-            qy = float(data[6])  # qy
-            qz = float(data[7])  # qz
-            qw = float(data[8])  # qw
+            if (i % 10 != 0):
+                continue
+            else:    
+                j += 1
+                data = line.strip().split()
+                index = j  # 第一列是从1开始的计数
+                timestamp = float(data[1])
+                x = float(data[2])  # UTM x
+                y = float(data[3])  # UTM y
+                z = float(data[4])  # UTM z
+                qx = float(data[5])  # qx
+                qy = float(data[6])  # qy
+                qz = float(data[7])  # qz
+                qw = float(data[8])  # qw
 
-            # 将四元数转换为 FRD 旋转矩阵
-            frd_rotation_matrix = quaternion_to_rotation_matrix([qx, qy, qz, qw])
+                # 将四元数转换为 FRD 旋转矩阵
+                frd_rotation_matrix = quaternion_to_rotation_matrix([qx, qy, qz, qw])
 
-            # 将 FRD 旋转矩阵转换为 FLU 旋转矩阵
-            flu_rotation_matrix = convert_frd_to_flu_rotation_matrix(frd_rotation_matrix)
+                # 将 FRD 旋转矩阵转换为 FLU 旋转矩阵
+                flu_rotation_matrix = convert_frd_to_flu_rotation_matrix(frd_rotation_matrix)
 
-            # 提取 FLU 旋转矩阵的行
-            row1 = flu_rotation_matrix[0]  # 第一行
-            row2 = flu_rotation_matrix[1]  # 第二行
-            row3 = flu_rotation_matrix[2]  # 第三行
+                # 提取 FLU 旋转矩阵的行
+                row1 = flu_rotation_matrix[0]  # 第一行
+                row2 = flu_rotation_matrix[1]  # 第二行
+                row3 = flu_rotation_matrix[2]  # 第三行
 
-            # 写入新文件
-            f_out.write(f"{index} {row1[0]} {row1[1]} {row1[2]} {x} {row2[0]} {row2[1]} {row2[2]} {y} {row3[0]} {row3[1]} {row3[2]} {z}\n")
+                # 写入新文件
+                f_out.write(f"{index} {row1[0]} {row1[1]} {row1[2]} {x} {row2[0]} {row2[1]} {row2[2]} {y} {row3[0]} {row3[1]} {row3[2]} {z}\n")
 
 if __name__ == "__main__":
     input_file = "odometry_loc.txt"
