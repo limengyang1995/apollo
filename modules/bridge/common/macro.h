459e974d34751866abd8e1e3ddda0d0a574817fe
dual_orin_develop_master <limengyang1995@126.com>
2025-06-17 13:16:26 +0800

    fix bridge perception obstacles problem

--- modules/bridge/common/macro.h
+++ modules/bridge/common/macro.h
@@ -33,6 +33,6 @@ namespace bridge {
   }                     \
   p = nullptr
 
-constexpr uint32_t FRAME_SIZE = 1024;
+constexpr uint32_t FRAME_SIZE = 8192;
 }  // namespace bridge
 }  // namespace apollo
